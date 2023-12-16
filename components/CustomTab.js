308ea22ec854bdc9aff42cc8a32339ade2f2574f
devkgm <gm.kim333@gmail.com>
2023-12-16 23:21:25 +0900

    first commit

--- components/CustomTab.js
+++
@@ -1,7 +0,0 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-const Tab = createBottomTabNavigator();
-
-export default function CustomTab(Screens) {
-    return <Tab.Navigator>{Screens.map((screen) => ({ screen }))}</Tab.Navigator>;
-}
--- components/Order.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, View } from 'react-native';
-
-export default Order = ({ product, quantity, price }) => {
-    return <View style={styles.container}></View>;
-};
-
-const styles = StyleSheet.create({
-    container: {},
-});
