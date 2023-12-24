96b14d633ae65df668c0327c752a8886adaefda8
devkgm <gm.kim333@gmail.com>
2023-12-24 17:59:27 +0900

    add logout check alert

--- components/Addon/LogoutButton.js
+++ components/Addon/LogoutButton.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, Alert } from 'react-native';
 import { storeData } from '../../store/storage';
 import { useContext } from 'react';
 import AuthContext from '../../store/AuthContext';
@@ -8,17 +8,23 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default LogoutButton = () => {
     const myAuthContext = useContext(AuthContext);
-    return (
-        <View style={AddonStyles.buttonContainer}>
-            <TouchableOpacity
-                style={AddonStyles.button}
-                onPress={() => {
+    const handleOnPress = () => {
+        Alert.alert('로그아웃', '로그아웃 하시겠습니까?', [
+            {
+                text: '로그아웃',
+                onPress: () => {
                     logOut();
                     myAuthContext.setIsLogin(false);
                     myAuthContext.setUser(null);
                     storeData('user', null);
-                }}
-            >
+                },
+            },
+            { text: '취소', style: 'cancel' },
+        ]);
+    };
+    return (
+        <View style={AddonStyles.buttonContainer}>
+            <TouchableOpacity style={AddonStyles.button} onPress={() => handleOnPress()}>
                 <Icon name="sign-out" size={16} color="#666" />
                 <Text style={AddonStyles.buttonText}>로그아웃</Text>
             </TouchableOpacity>
