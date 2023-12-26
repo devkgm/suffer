6d0247497c34af6072910f5d3487f81ae1ea22fb
devkgm <gm.kim333@gmail.com>
2023-12-26 23:48:40 +0900

    update auth struct

--- services/signIn.js
+++ services/signIn.js
@@ -1,17 +1,25 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from './firebaseConfig';
 import { storeData } from '../store/storage';
 import { Alert } from 'react-native';
 
-export default signIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            storeData('user', user);
-            return user;
-        })
-        .catch((error) => {
-            Alert.alert('이메일 혹은 비밀번호를 확인해주세요.');
-            console.error(new Error(error));
+export default signIn = async (email, password) => {
+    try {
+        const response = await fetch('http://192.168.45.212:3000/login', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({ email: email, password: password }),
         });
+        const data = await response.json();
+        console.log(data);
+        storeData('user', data.user);
+        storeData('accessToken', data.accessToken);
+        storeData('refreshToken', data.refreshToken);
+        return data.user;
+    } catch (error) {
+        Alert.alert('이메일 혹은 비밀번호를 확인해주세요.');
+        console.error(new Error(error));
+    }
 };
